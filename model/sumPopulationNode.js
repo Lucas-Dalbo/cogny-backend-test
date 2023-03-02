930b8e5c16e18929b0e27c588b6a2a0ba0a0f29e
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-02 14:29:32 -0300

    feat: Soma usando a memoria do node - Função sumPopulationNode; - Utiliza reduce para realizar a soma das populações de 2018, 2019 e 2020

---
+++ model/sumPopulationNode.js
@@ -0,0 +1,22 @@
+const { findAllActive  } = require('./findAllActive');
+
+async function sumPopulationNode(db) {
+  console.log("Calculando a soma das Populações de 2018, 2019 e 2020...");
+  try {
+    const [result] = await findAllActive(db);
+    const data = result.doc_record;
+    const sum = data.reduce((acc, crr) => {
+      const { Population, Year } = crr;
+      if (2018 <= Year && Year <= 2020) {
+        return acc = acc + Population;
+      }
+      return acc;
+    }, 0);
+  
+    return `A soma é de ${sum}`;
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+module.exports = { sumPopulationNode };

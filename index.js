0f971e3cab14c07ca46c799591a874a8fd09ad11
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-02 14:35:36 -0300

    feat/refactor: Interface do Usiário - Corrige erros de gramática. - Adiciona opção 3, para calcular a soma de populações.

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const { dbConnector } = require('./db');
 const { readLineAsync, readline } = require('./interface');
 const { fetchDataToDB } = require('./model/fetchDataToDB');
 const { findAllActive } = require('./model/findAllActive');
+const { sumPopulationNode } = require('./model/sumPopulationNode');
 
 
-// Call start
-(async () => {
+async function main() {
     const db = await dbConnector();
     const actions = {
         "1": fetchDataToDB,
         "2": findAllActive,
+        "3": sumPopulationNode,
     };
 
     const response = await readLineAsync(
         "\nO que deseja fazer?\n" +
         "   1. Consultar a API e salvar os dados no banco de dados.\n" +
-        "   2. Listar infromações do banco de dados.\n" +
+        "   2. Listar informações do banco de dados.\n" +
+        "   3. Calcular a soma das populações de 2018, 2019 e 2020\n" +
         "   0. Sair;\n"
     );
     readline.close();
@@ -24,9 +26,12 @@ const { findAllActive } = require('./model/findAllActive');
         console.log("Até mais tarde! ;)");
     } else {
         try {
-            await actions[response](db);
+            const actionResult = await actions[response](db)
+            console.log(actionResult);
         } catch {
             console.log("Funcionalidade indisponível");
         }
     }
-})();
+}
+
+main();

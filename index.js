2a0cae30d556a5889f6239c0769760f399fa7d39
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-02 12:03:48 -0300

    FEAT: Implementação da interface Funcionalidades: - 1. Consultar a API e salvar informações no bd. - 2. Listar informações do bacno (temporaria). - 0. Sair.

--- index.js
+++ index.js
@@ -1,86 +1,32 @@
-const { DATABASE_SCHEMA, DATABASE_URL, SHOW_PG_MONITOR } = require('./config');
-const massive = require('massive');
-const monitor = require('pg-monitor');
+const { dbConnector } = require('./db');
+const { readLineAsync, readline } = require('./interface');
+const { fetchDataToDB } = require('./model/fetchDataToDB');
+const { findAllActive } = require('./model/findAllActive');
+
 
 // Call start
 (async () => {
-    console.log('main.js: before start');
+    const db = await dbConnector();
+    const actions = {
+        "1": fetchDataToDB,
+        "2": findAllActive,
+    };
 
-    const db = await massive({
-        connectionString: DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-    }, {
-        // Massive Configuration
-        scripts: process.cwd() + '/migration',
-        allowedSchemas: [DATABASE_SCHEMA],
-        whitelist: [`${DATABASE_SCHEMA}.%`],
-        excludeFunctions: true,
-    }, {
-        // Driver Configuration
-        noWarnings: true,
-        error: function (err, client) {
-            console.log(err);
-            //process.emit('uncaughtException', err);
-            //throw err;
+    const response = await readLineAsync(
+        "\nO que deseja fazer?\n" +
+        "   1. Consultar a API e salvar os dados no banco de dados.\n" +
+        "   2. Listar infromações do banco de dados.\n" +
+        "   0. Sair;\n"
+    );
+    readline.close();
+ 
+    if (response === "0") {
+        console.log("Até mais tarde! ;)");
+    } else {
+        try {
+            await actions[response](db);
+        } catch {
+            console.log("Funcionalidade indisponível");
         }
-    });
-
-    if (!monitor.isAttached() && SHOW_PG_MONITOR === 'true') {
-        monitor.attach(db.driverConfig);
     }
-
-    const execFileSql = async (schema, type) => {
-        return new Promise(async resolve => {
-            const objects = db['user'][type];
-
-            if (objects) {
-                for (const [key, func] of Object.entries(objects)) {
-                    console.log(`executing ${schema} ${type} ${key}...`);
-                    await func({
-                        schema: DATABASE_SCHEMA,
-                    });
-                }
-            }
-
-            resolve();
-        });
-    };
-
-    //public
-    const migrationUp = async () => {
-        return new Promise(async resolve => {
-            await execFileSql(DATABASE_SCHEMA, 'schema');
-
-            //cria as estruturas necessarias no db (schema)
-            await execFileSql(DATABASE_SCHEMA, 'table');
-            await execFileSql(DATABASE_SCHEMA, 'view');
-
-            console.log(`reload schemas ...`)
-            await db.reload();
-
-            resolve();
-        });
-    };
-
-    try {
-        await migrationUp();
-
-        //exemplo de insert
-        const result1 = await db[DATABASE_SCHEMA].api_data.insert({
-            doc_record: { 'a': 'b' },
-        })
-        console.log('result1 >>>', result1);
-
-        //exemplo select
-        const result2 = await db[DATABASE_SCHEMA].api_data.find({
-            is_active: true
-        });
-        console.log('result2 >>>', result2);
-
-    } catch (e) {
-        console.log(e.message)
-    } finally {
-        console.log('finally');
-    }
-    console.log('main.js: after start');
 })();
